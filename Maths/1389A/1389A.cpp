94c49e5944f2e296553c610c8fb41ee7214a72b8
HemantKr3002 <147808593+HemantKr3002@users.noreply.github.com>
2023-10-14 11:20:05 +0530

    Update 1389A.cpp

--- Maths/1389A/1389A.cpp
+++ Maths/1389A/1389A.cpp
@@ -9,7 +9,9 @@ int main() {
     while(t--){
         int l,r;
         cin >> l >> r;
+        //if there exists l * 2 in the range[l,r] and as the lcm(l,l*2) = l * 2 itself which would be the smallest possible answer
         if(l * 2 <= r) cout << l << " " << l * 2 << endl;
+        //if smallest possible answer doesn't exist it implies there is no possible answer
         else cout << -1 << " " << -1 << endl;
     }
     return 0;
