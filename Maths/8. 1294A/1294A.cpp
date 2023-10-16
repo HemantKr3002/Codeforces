2a8b975a0f6d4dbc12d892cd84ffdce59052d83e
HemantKr3002 <147808593+HemantKr3002@users.noreply.github.com>
2023-10-16 20:27:35 +0530

    Update 1294A.cpp

--- Maths/8. 1294A/1294A.cpp
+++ Maths/8. 1294A/1294A.cpp
@@ -10,25 +10,31 @@ int main() {
     while(t--){
         int a,b,c,n;
         cin >> a >> b >> c >> n;
-        long long total = a + b + c + n;
-        int maxi = max({a,b,c});
-        int mini = min({a,b,c});
-        if(total % 3 == 0 && total / 3 > maxi) cout << "YES" << endl;
-        else if(total % 3 == 0 && total / 3 == maxi){
-            if((a == b || a == c) && a == maxi){
-                if(n == maxi - mini) cout << "YES" << endl;
-                else cout << "NO" << endl;
-            }
-            else if(b == c && b == maxi){
-                if(n == maxi - mini) cout << "YES" << endl;
-                else cout << "NO" << endl;
-            }
-            else{
-                if((total - maxi) % 2 == 0) cout << "YES" << endl;
-                else cout << "NO" << endl;
-            }
-        }
-        else cout << "NO" << endl;
+        //Approach 1:
+        // long long total = a + b + c + n;
+        // int maxi = max({a,b,c});
+        // int mini = min({a,b,c});
+        // if(total % 3 == 0 && total / 3 > maxi) cout << "YES" << endl;
+        // else if(total % 3 == 0 && total / 3 == maxi){
+        //     if((a == b || a == c) && a == maxi){
+        //         if(n == maxi - mini) cout << "YES" << endl;
+        //         else cout << "NO" << endl;
+        //     }
+        //     else if(b == c && b == maxi){
+        //         if(n == maxi - mini) cout << "YES" << endl;
+        //         else cout << "NO" << endl;
+        //     }
+        //     else{
+        //         if((total - maxi) % 2 == 0) cout << "YES" << endl;
+        //         else cout << "NO" << endl;
+        //     }
+        // }
+        // else cout << "NO" << endl;
+
+        //Approach 2:
+        int sum = (a+b+c+n);
+        if(sum  % 3 != 0 || sum/3<a || sum/3<b || sum/3<c) cout << "NO" << endl;
+        else cout << "YES" << endl;
     }
     return 0;
 }
