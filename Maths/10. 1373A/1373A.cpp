172db4ed6e338ea70af12e2848a6b9a6bd7d4ae5
HemantKr3002 <147808593+HemantKr3002@users.noreply.github.com>
2023-10-17 19:54:33 +0530

    Create 1373A.cpp

---
+++ Maths/10. 1373A/1373A.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        cout << (n - 1) / 2 + 1 << endl;
+    }
+    return 0;
+}
